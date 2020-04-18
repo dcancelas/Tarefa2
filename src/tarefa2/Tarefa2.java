edaf8997cacdf1101b0d66e0f5ab8a5d2ae1e043
dcancelas <55793127+DanielCancelas@users.noreply.github.com>
2020-04-18 17:21:22 +0200

    Initial commit

---
+++ src/tarefa2/Tarefa2.java
@@ -0,0 +1,17 @@
+
+package tarefa2;
+
+/**
+ *
+ * @author dcancelas
+ */
+public class Tarefa2 {
+
+    public static void main(String[] args) {
+        Ventana ven = new Ventana();
+        ven.setVisible(true);
+        Ventana2 ven2 = new Ventana2();
+        ven2.setVisible(true);
+    }
+    
+}
